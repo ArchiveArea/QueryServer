5f06ccca97936aeed7793227270d850d7db67dbc
Taylor.R <nguyenhieucoder47@gmail.com>
2023-06-05 10:17:25 +0700

    Format file

--- src/Main.php
+++ src/Main.php
@@ -14,11 +14,13 @@ use NhanAZ\QueryServer\libs\libpmquery\PmQueryException;
 use pocketmine\console\ConsoleCommandSender;
 use pocketmine\Server;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements Listener
+{
 
-	public array $players = [];
+    public array $players = [];
 
-	public static function qrs($sender, $query): void {
+    public static function qrs($sender, $query): void
+    {
         $keys = [
             "GameName" => "§e>§f GameName:§a ",
             "HostName" => "§e>§f HostName:§r ",
@@ -42,7 +44,8 @@ class Main extends PluginBase implements Listener {
         }
     }
 
-    public static function logInfo($status): void {
+    public static function logInfo($status): void
+    {
         $sender = new ConsoleCommandSender(Server::getInstance(), Server::getInstance()->getLanguage());
         $serverInfo = $status->ip . ":" . $status->port;
         if ($status->online) {
@@ -81,7 +84,7 @@ class Main extends PluginBase implements Listener {
                         $value = $value->$key;
                     }
                     $output = ($value == null) ? "§cError or has blocked queries!" : $value;
-                    if(is_array($output)) {
+                    if (is_array($output)) {
                         $output = implode("§f,§a ", $output);
                     }
                     $sender->sendMessage("§e>§f $label:§a " . $output);
@@ -110,47 +113,47 @@ class Main extends PluginBase implements Listener {
         }
     }
 
-	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
-		if ($cmd->getName() == "query") {
-			if (!isset($args[0])) {
-				$sender->sendMessage("§e>§c Usage: /query §b<domain/ip:port>§c to query certain server information");
-				$sender->sendMessage("§e>§c Example: /query §b0.0.0.0:19132 §cor §b/query goole.com:19132");
-				return true;
-			}
-			if (!$sender instanceof Player) {
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
+    {
+        if ($cmd->getName() == "query") {
+            if (!isset($args[0])) {
+                $sender->sendMessage("§e>§c Usage: /query §b<domain/ip:port>§c to query certain server information");
+                $sender->sendMessage("§e>§c Example: /query §b0.0.0.0:19132 §cor §b/query goole.com:19132");
+                return true;
+            }
+            if (!$sender instanceof Player) {
                 $this->getServer()->getAsyncPool()->submitTask(new QueryTask($args[0]));
-
-			} else {
-				$sender->sendMessage("§e>§c Please use the command on the console!");
-			}
-			return true;
-		}
-		if ($cmd->getName() == "querys") {
-			if ($sender instanceof Player) {
-				$sender->sendMessage("§e>§c Please use the command on the console!");
-				return true;
-			}
-			if (!isset($args[0])) {
-				$sender->sendMessage("§e>§f Error:§c You have not entered the IP/Domain of the server you want to query!");
-				$sender->sendMessage("§e>§c Usage: /query §b<domain/ip> <port>§c to query certain server information");
-				$sender->sendMessage("§e>§c Example: /query §b0.0.0.0 19132 §cor §b/query goole.com 19132");
-				return true;
-			}
-			if (!isset($args[1])) {
-				$sender->sendMessage("§e>§f Error:§c You have not entered the Port of the server you want to query!");
-				$sender->sendMessage("§e>§c Usage: /query §b<domain/ip> <port>§c to query certain server information");
-				$sender->sendMessage("§e>§c Example: /query §b0.0.0.0 19132 §cor §b/query goole.com 19132");
-				return true;
-			}
-			try {
-				$query = PMQuery::query($args[0], (int)$args[1]);
-				Main::qrs($sender, $query);
-			} catch (PmQueryException $e) {
-				$sender->sendMessage("§e>§c The server is offline or has blocked queries!");
-				$sender->sendMessage("§e>§f Possible error:§c Your IP does not open the port or the device does not match!");
-			}
-			return true;
-		}
-		return true;
-	}
+            } else {
+                $sender->sendMessage("§e>§c Please use the command on the console!");
+            }
+            return true;
+        }
+        if ($cmd->getName() == "querys") {
+            if ($sender instanceof Player) {
+                $sender->sendMessage("§e>§c Please use the command on the console!");
+                return true;
+            }
+            if (!isset($args[0])) {
+                $sender->sendMessage("§e>§f Error:§c You have not entered the IP/Domain of the server you want to query!");
+                $sender->sendMessage("§e>§c Usage: /query §b<domain/ip> <port>§c to query certain server information");
+                $sender->sendMessage("§e>§c Example: /query §b0.0.0.0 19132 §cor §b/query goole.com 19132");
+                return true;
+            }
+            if (!isset($args[1])) {
+                $sender->sendMessage("§e>§f Error:§c You have not entered the Port of the server you want to query!");
+                $sender->sendMessage("§e>§c Usage: /query §b<domain/ip> <port>§c to query certain server information");
+                $sender->sendMessage("§e>§c Example: /query §b0.0.0.0 19132 §cor §b/query goole.com 19132");
+                return true;
+            }
+            try {
+                $query = PMQuery::query($args[0], (int)$args[1]);
+                Main::qrs($sender, $query);
+            } catch (PmQueryException $e) {
+                $sender->sendMessage("§e>§c The server is offline or has blocked queries!");
+                $sender->sendMessage("§e>§f Possible error:§c Your IP does not open the port or the device does not match!");
+            }
+            return true;
+        }
+        return true;
+    }
 }
