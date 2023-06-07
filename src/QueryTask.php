10ae506d06f84d73ceab33e5c1035af7288c7a2c
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-06-07 16:19:31 +0700

    How to fix?

--- src/QueryTask.php
+++ src/QueryTask.php
@@ -6,6 +6,7 @@ namespace NhanAZ\QueryServer;
 
 use pocketmine\scheduler\AsyncTask;
 use pocketmine\utils\Internet;
+use pocketmine\utils\InternetException;
 
 class QueryTask extends AsyncTask {
 
@@ -16,13 +17,15 @@ class QueryTask extends AsyncTask {
 
 	public function onRun(): void {
 		try {
+			$status = null;
 			$status = Internet::getURL("https://api.mcsrvstat.us/2/" . $this->info);
-		} catch (\pocketmine\utils\InternetException $e) {
+		} catch (InternetException $e) {
+			var_dump($e->getMessage());
 			var_dump("§e>§f Error:§c Your IP does not open the port or the device does not match!");
 			var_dump("§e>§c Try another query method using §b/querys");
-			$status = json_decode($status->getBody());
-			$this->setResult($status);
 		}
+		$status = json_decode($status->getBody());
+		$this->setResult($status);
 	}
 
 	public function onCompletion(): void {
